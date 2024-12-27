3dc860ce7505fd0a0e38d2049448155a5d82b0ee
Edily Mora <edilyrosa@gmail.com>
2024-12-27 19:14:54 -0400

    first commit

---
+++ throttling.js
@@ -0,0 +1,27 @@
+function throttle(func, limit) {
+    let lastFunc;
+    let lastRan;
+
+    return function() {
+        const context = this;
+        const args = arguments;
+
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(function() {
+                if ((Date.now() - lastRan) >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    };
+}
+
+
+window.addEventListener('click', throttle(function() {
+    console.log('Scroll event triggered');
+}, 4000)); // Limita a una ejecución cada 100 ms
